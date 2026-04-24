664c375a0af79f80cc23bbe33ff81c2f232b05f6
The Cake House Ja-Ela <thecakehousejaela@gmail.com>
2026-04-24 22:51:53 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'cake-house-v1';
+const assets = [
+  '/',
+  'index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install Service Worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch events
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
